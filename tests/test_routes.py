41198c1b51f0bdb18374bc09e505e8040649ea16
muhddanishaiman <muhdnish2572@gmail.com>
2025-07-23 22:48:57 -0400

    update account completed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -129,6 +129,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
@@ -142,6 +143,7 @@ class TestAccountService(TestCase):
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
@@ -150,3 +152,17 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json = test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json = new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
